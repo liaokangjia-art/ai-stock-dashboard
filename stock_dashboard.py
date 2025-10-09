a989b5acd6ee6e5195b5862828b36e2c8899e0d3
liaokangjia-art <liaokangjia@gmail.com>
2025-10-09 16:13:41 +0800

    Update stock_dashboard.py

--- stock_dashboard.py
+++ stock_dashboard.py
@@ -495,7 +495,7 @@ def main():
     stock_choice = st.sidebar.selectbox(
         "🏢 Select Stock:",
         options=['Custom'] + list(popular_stocks.keys()),
-        index=1
+        index=2
     )
     
     if stock_choice == 'Custom':
